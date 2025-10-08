004c67367b12d7a14d8631ae2618214ceb6695bd
Faridzy555 <faridfebrianto555@gmail.com>
2025-10-08 11:16:02 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS06.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS06 {
+    public static void main(String[] args) {
+        Scanner Farid = new Scanner(System.in);
+
+        System.out.println("Cetak KRS Siakad");
+        System.out.println("Apakah UKT sudah lunas? (true/false)");
+        boolean ukt_lunas = Farid.nextBoolean();
+
+        if (ukt_lunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
